bccf02382a86266475d444a67140af9d49fa3921
Artur Mirgaliev <37755482+FoxProklya@users.noreply.github.com>
2018-06-27 10:37:07 +0300

    Update Sexy.py

--- Sexy.py
+++ Sexy.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import xlsxwriter
+
 k1 = 5
 k2 = 8
 k3 = k2*k1-99
@@ -47,3 +49,11 @@ df.loc['Q4'] = (df.loc['Q1'] * df.loc['98'] / (df.loc[k3] + 22)) - df.loc['9']
 print('')
 
 print(df)
+
+writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
+
+# Convert the dataframe to an XlsxWriter Excel object.
+df.to_excel(writer, sheet_name='Sheet1')
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()

e29e6ce9ade13349bb65e191e183dee255ca797c
Artur Mirgaliev <37755482+FoxProklya@users.noreply.github.com>
2018-06-29 09:44:58 +0300

    Update testlab3.py

--- testlab3.py
+++ testlab3.py
@@ -5,47 +5,7 @@ import xlsxwriter
 import psycopg2
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://(NAMEaccaunt):#########@(SERVER):(HOST)/(NAMEDB)',echo=False)
-n = int(input('колличество строк: '))
-
-df = pd.DataFrame({
-				    #'Номер пробы'     : [],
-				    #'Номер скважины'  : [],
-				    #'гл. *** м.'      : [],
-				    #'кальций-ион'     : [],
-				    #'магний-ион'      : []
-				    'q1'      : [],
-				    'q2'      : [],
-				    'q3'      : [],
-				    'q4'      : [],
-				    'q5'      : []
-	
-	}
-	)
-	
-
-for i in range(n):
-	#k1 = int  (input('Номер пробы: '     ))
-	#k2 = int  (input('Номер скважины: '  ))
-	#k3 = float(input('гл. *** м.: '      ))
-	#k4 = int  (input('кальций-ион: '     ))
-	#k5 = int  (input('магний-ион: '      ))
-	k1 = int  (input('Номер пробы: '     ))
-	k2 = int  (input('Номер скважины: '  ))
-	k3 = float(input('гл. *** м.: '      ))
-	k4 = int  (input('кальций-ион: '     ))
-	k5 = int  (input('магний-ион: '      ))
-
-	df.loc[i+1] = k1, k2, k3, k4, k5
-
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import xlsxwriter
-import psycopg2
-from sqlalchemy import create_engine
-
-engine = create_engine('postgresql://artur:2011Milki532678@endiny.me:5432/artur_test',echo=False)
+engine = create_engine('postgresql://#:############@#####.###:5432/######',echo=False)
 n = int(input('колличество строк: '))
 
 df = pd.DataFrame({

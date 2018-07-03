bd2b34c599357cc02878f40c484e30db9c6e5d79
Artur Mirgaliev <37755482+FoxProklya@users.noreply.github.com>
2018-07-03 18:54:27 +0300

    Create alfa.py

---
+++ alfa.py
@@ -0,0 +1,83 @@
+# Подключение библеотек
+import   numpy       as np
+import   pandas      as pd
+import   seaborn     as sns
+import   xlsxwriter
+import   psycopg2
+
+from sqlalchemy   import create_engine
+#________________________________
+
+#Подключение к SQL
+engine = create_engine
+#________________________________
+
+#введение колличество строк для заполнения базы
+n = int(input('колличество строк: '))
+klist = []
+#создание датафрейма, введение слобцов базы
+df = pd.DataFrame({
+				    'Номер пробы'     : [],
+				    'Номер скважины'  : [],
+				    'гл. *** м.'      : [],
+				   	'кальций-ион'     : [],
+				   	'магний-ион'      : []
+#					'q1'      : [],
+#				    'q2'      : [],
+#				    'q3'      : [],
+#				   	'q4'      : [],
+#				   	'q5'      : [],
+
+	
+	}
+	)
+#________________________________
+
+#data = pd.read_sql('SELECT * FROM test', engine)
+
+#print(data)
+
+#вводимые данные
+for i in range(n):
+	k1 = int  (input('Номер пробы: '     ))
+	k2 = int  (input('Номер скважины: '  ))
+	k3 = float(input('гл. *** м.: '      ))
+	k4 = int  (input('кальций-ион: '     ))
+	k5 = int  (input('магний-ион: '      ))
+	klist += [k1]
+#k1 = int  (input('Номер пробы: '     ))
+#k2 = int  (input('Номер скважины: '  ))
+#k3 = float(input('гл. *** м.: '      ))
+#k4 = int  (input('кальций-ион: '     ))
+#k5 = int  (input('магний-ион: '      ))
+
+#df.index = [k1]
+	df.loc[i+1] = k1, k2, k3, k4, k5
+
+#df.index = klist
+df['Операция вычисления'] = df['Номер скважины'] - df['кальций-ион'] 
+
+print(df)
+
+#if k1 in data['Номер пробы'] :
+
+
+
+data = pd.read_sql('SELECT * FROM test', engine)
+
+print(data)
+
+data.update(df)
+
+print(data)
+
+	
+data.to_sql(name='test', con=engine, if_exists = 'replace', index=False)
+
+writer = pd.ExcelWriter('test.xlsx', engine='xlsxwriter')
+
+# Convert the dataframe to an XlsxWriter Excel object.
+df.to_excel(writer, sheet_name='testsheet')
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()

fa4931646d90e91baa6a33cf4db9b9b6267c3158
Artur Mirgaliev <37755482+FoxProklya@users.noreply.github.com>
2018-07-13 12:18:05 +0300

    Update Квасное_сусло.py

--- Квасное_сусло.py
+++ Квасное_сусло.py
@@ -1,4 +1,4 @@
-# Подключение библеотек 
+# Подключение библеотек______
 import numpy as np 
 import pandas as pd 
 import seaborn as sns 
@@ -7,25 +7,27 @@ import psycopg2
 import sqlite3 
 
 from sqlalchemy import create_engine 
-#________________________________ 
+#____________________________ 
 
-#Подключение к SQL
-engine = create_engine('postgresql://login:password@server:port/bazatable',echo=False) #для непадения
+#Подключение к SQL___________
+engine = create_engine('',echo=False)
 
-engine = psycopg2.connect( host='', user='', password='', dbname='') #для курсора
-#________________________________
+engine = psycopg2.connect( host='', user='', password='', dbname='') 
+#____________________________
 
 #введение колличество строк для заполнения базы 
 #n = int(input('колличество строк: ')) 
 klist = [] #список на будущее 
 
-#Ввод значений
+#Ввод значений_______________
 k1 = int  (input('Номер пробы: '     ))
 k2 = int  (input('Номер скважины: '  ))
 k3 = float(input('гл. *** м.: '      ))
 k4 = int  (input('кальций-ион: '     ))
 k5 = int  (input('магний-ион: ' ))
 k6 = k5 - k4
+#____________________________
+
 #создание датафрейма, введение слобцов базы
 df = pd.DataFrame({
 		'Номер пробы'         : [k1],
@@ -33,18 +35,23 @@ df = pd.DataFrame({
 		'гл. *** м.'          : [k3],
 		'кальций-ион'         : [k4],
 		'магний-ион'          : [k5],
-            	'Операция вычисления' : [k6]
+    'Операция вычисления' : [k6]
 
 	
     	}
   	)
 print(df)
-#________________________________
+#____________________________
+
 #МАГИЯ
+
+#READ AND CHANGE INDEX_______
 data = pd.read_sql('SELECT * FROM test', engine)
 data.index = data['Номер пробы']
 print(data) #Проверка таблицы
+#____________________________
 
+#INSERT OR UPDATE____________
 if k1 not in data.index:
   #Если нету то ИНСЕРТ
   print('В бездну список') #Проверка условия1
@@ -77,39 +84,30 @@ else:
   )
 #____________________________
 
-#Проверка работы со строкой
+
+#Проверка работы со строкой__
 cursor = engine.cursor()
 cursor.execute( 'SELECT test FROM test WHERE "Номер пробы" = %s' , [k1]) 
 results = cursor.fetchall()
 print(results)
+#____________________________
 
 #Проверка базы после махинаций
 data1 = pd.read_sql('SELECT * FROM test', engine)
 print(data1)
+#____________________________
 
+#END WORK SQL________________
+engine.commit() #SAVE
+engine.close()  #CLOSE
+#____________________________
 
-#SAVE
-engine.commit()
-
-#CLOSE
-engine.close()
-
-#EXCEL
-
+#EXCEL_______________________
 writer = pd.ExcelWriter('test.xlsx', engine='xlsxwriter')
-
 # Convert the dataframe to an XlsxWriter Excel object.
-
-#BASE OLD
-data.to_excel(writer, sheet_name='OldBase')
-
-#NEW ROW
-df.to_excel(writer, sheet_name='NewRow')
-
-
-#full BASE
-data1.to_excel(writer, sheet_name='FullBase')
-
-
+data.to_excel  (writer, sheet_name='OldBase' )    #BASE OLD
+df.to_excel    (writer, sheet_name='NewRow'  )    #NEW ROW
+data1.to_excel (writer, sheet_name='FullBase')    #full BASE
 # Close the Pandas Excel writer and output the Excel file.
 writer.save()
+#____________________________
